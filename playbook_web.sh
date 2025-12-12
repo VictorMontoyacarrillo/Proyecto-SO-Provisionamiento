57c8f7cc848e4cb32dc88893d96dddc765961223
Victor Montoya <vamontoya@inorca.com.co>
2025-12-12 10:53:21 -0500

    Proyecto: agregar sitio web estático (index + imagen) y archivos de proyecto

--- playbook_web.sh
+++ playbook_web.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-
 echo "=== Instalando servidor web Nginx ==="
 sudo apt update -y
 sudo apt install -y nginx
 
-echo "=== Copiando el index.html personalizado ==="
+echo "=== Copiando archivos del sitio web ==="
 sudo cp /vagrant/index.html /var/www/html/index.html
+sudo cp /vagrant/logo.png /var/www/html/logo.png
+
+echo "=== Ajustando permisos ==="
+sudo chmod 644 /var/www/html/*
 
 echo "=== Reiniciando Nginx ==="
 sudo systemctl restart nginx
